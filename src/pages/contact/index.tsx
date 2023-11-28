10e0e2fb926a3be2041f07d8e7d7e47ff169e63d
Daniel-Alves2017 <daniel.alves.oliveira@outlook.com.br>
2023-11-28 00:16:28 -0300

    CORREÇÃO 404

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,21 +5,9 @@ const Contact = () => {
   return (
     <aside className="contact">
       <div className="social">
-        <a
-          href="https://www.instagram.com/teamarmanni_diego"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="highlight-link"
-        >
-          <FaInstagram
-            size={32}
-            style={{
-              color: 'white',
-              borderRadius: 6,
-              background: `radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)`,
-            }}
-          />
-          <span>@teamarmanni_diego</span>
+        
+        
+          <span>@GymWoman</span>
         </a>
         <a href="#" rel="noopener noreferrer" className="highlight-link">
           <FaPhoneAlt
@@ -31,10 +19,10 @@ const Contact = () => {
               background: 'blue',
             }}
           />
-          <span>(31) 98353-6211</span>
+          <span>(11) 963988878</span>
         </a>
         <a
-          href="https://wa.me/5522989652365"
+          href="https://wa.me/5511963988878"
           target="_blank"
           rel="noopener noreferrer"
           className="highlight-link"
@@ -52,8 +40,7 @@ const Contact = () => {
       </div>
       <div className="label">
         <label>
-          Av. Professor José Maurício, 381 - Lindéia, Belo Horizonte - MG,
-          32400-000
+        R. Marquês de Paranaguá, 111 - Consolação, São Paulo - SP, 01303-050
         </label>
       </div>
     </aside>
