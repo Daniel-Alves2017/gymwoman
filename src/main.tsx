b6ace1bc8b2a37887844f8d4ae537c29be7d2646
Daniel-Alves2017 <daniel.alves.oliveira@outlook.com.br>
2023-11-27 21:16:14 -0300

    alterar titulo

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Title>GymWoman</Title>
     <App />
   </React.StrictMode>
 );
