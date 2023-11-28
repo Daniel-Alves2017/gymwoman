9a45fe2cda750714740c3ff5a84dcee1dc1fcd18
Daniel-Alves2017 <daniel.alves.oliveira@outlook.com.br>
2023-11-28 00:17:57 -0300

    corrreçao

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,8 +5,20 @@ const Contact = () => {
   return (
     <aside className="contact">
       <div className="social">
-        
-        
+        <a
+          href="https://www.instagram.com/Gymwoman"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="highlight-link"
+        >
+          <FaInstagram
+            size={32}
+            style={{
+              color: 'white',
+              borderRadius: 6,
+              background: `radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)`,
+            }}
+          />
           <span>@GymWoman</span>
         </a>
         <a href="#" rel="noopener noreferrer" className="highlight-link">
@@ -40,7 +52,7 @@ const Contact = () => {
       </div>
       <div className="label">
         <label>
-        R. Marquês de Paranaguá, 111 - Consolação, São Paulo - SP, 01303-050
+           R. Marquês de Paranaguá, 111 - Consolação, São Paulo - SP, 01303-050
         </label>
       </div>
     </aside>
